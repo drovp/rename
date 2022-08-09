bdaf5df4f944dad8e3f8ca3dc6176ccf196af620
tomasklaen <tomas.klaen@gmail.com>
2022-08-09 10:04:34 +0200

    fix: external links not opening in preview

--- src/preview.tsx
+++ src/preview.tsx
@@ -1,7 +1,8 @@
+import {shell} from 'electron';
 import * as Path from 'path';
 import {h, render} from 'preact';
 import {getPayload, resolve} from '@drovp/utils/modal-window';
-import {eem} from 'lib/utils';
+import {eem, isOfType, uid} from 'lib/utils';
 import {PreparatorPayload} from './';
 import {App} from 'components/App';
 import {Spinner} from 'components/Spinner';
@@ -10,18 +11,38 @@ import {Vacant} from 'components/Vacant';
 const container = document.getElementById('app-container')!;
 const INSTRUCTIONS_PATH = Path.resolve(__dirname, '../instructions.md');
 
+// Dev shortcuts
 window.addEventListener('keydown', (event) => {
 	switch (event.key) {
+		// Reload window
 		case 'F5':
 			window.location.reload();
 			break;
 
+		// Reload styles
 		case 'F6':
+			for (const link of document.querySelectorAll<HTMLLinkElement>('link[rel=stylesheet]')) {
+				link.href = link.href.replace(/\?\w+$/, '') + `?${uid()}`;
+			}
+			break;
+
+		// Toggle theme
+		case 'F8':
 			const currentTheme = document.documentElement.dataset.theme;
 			document.documentElement.dataset.theme = currentTheme === 'dark' ? 'light' : 'dark';
 			event.preventDefault();
 			break;
 	}
+});
+
+// Anchor links opening
+window.addEventListener('click', (event) => {
+	if (!isOfType<HTMLElement>(event.target, event.target != null)) return;
+	const anchor = event.target.closest<HTMLAnchorElement>('a[href]');
+	const href = anchor?.href;
+	if (!href) return;
+	event.preventDefault();
+	if (href.startsWith('http')) shell.openExternal(href);
 });
 
 render(<Spinner />, container);
